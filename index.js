8c799154cde5f899fb26d021b540185ef1a1e47b
Kameron Ashley <kameronashley@gmail.com>
2021-09-27 21:40:16 -0700

    commit tree 33aea1075986b79f6b20b3799791631225671ebb

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-main.remove();
+// const newElement = document.createElement('div')
+// document.body.append(newElement)
 
-const newHeader = document.createElement('h1');
-newHeader.setAttribute('id', 'victory');
-newHeader.textContent = ("Kameron is the champion");
+// document.body.append(element)
+
+// const ul = document.createElement('ul');
+// for (let i = 0; i < 3; i++) {
+//     const li = document.createElement('li')
+//     li.textContent = (i + 1).toString();
+//     ul.append(li)
+// }
+// createElement.append(ul)
+
+let main = document.querySelector('main#main') //found a <main> tag with id of 'main' and removed it
+main.remove()
+
+const newHeader = document.createElement('h1') //created an element <h1> and assigned it a variable newHeader. Gave it an id of victory
+newHeader.id = 'victory'
+newHeader.textContent = 'Kameron is the champion'
 
